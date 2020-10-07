808f58cd16b453d290f3ea3063de82d342aadf70
YingShao <ying511307543@163.com>
2020-10-07 21:05:48 +0800

    springboot第一天（构建一个项目并启动）

---
+++ zhst_mall/src/main/java/com/zhst/zhst_mall/ZhstMallApplication.java
@@ -0,0 +1,13 @@
+package com.zhst.zhst_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhstMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhstMallApplication.class, args);
+    }
+
+}
---
+++ zhst_mall/src/test/java/com/zhst/zhst_mall/ZhstMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhst.zhst_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhstMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
